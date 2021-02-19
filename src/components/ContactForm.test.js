a2a06f3741802e8244bd23fb524db8adbea08070
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-02-18 21:18:57 -0500

    Completed the requirements for MVP

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { render, screen } from "@testing-library/react";
 import ContactForm from './ContactForm.js';
-import userEvent from '@testing-library/user-event';
+import userEvent from "@testing-library/user-event";
+
 
 test('Making sure that the Contact Form component renders correctly', () => {
     render(<ContactForm />)
 })
 
-test('When a person fills out and submits the contact form, a new contact is added and displayed', () => {
+test('When a user fills out and submit the form, a new contact is created and displayed', async () => {
+    //Arrange phase of the test
     render(<ContactForm />);
-    // Arrange phase of the test
     const firstNameInput = screen.getByLabelText(/first name/i);
     const lastNameInput = screen.getByLabelText(/last name/i);
     const emailInput = screen.getByLabelText(/email/i);
     const messageInput = screen.getByLabelText(/message/i);
+    const submitButton = screen.getByRole('button', { name: /submit/i });
 
-    // Act phase of the test - This is where an action will be simulated on elements of the component
-    userEvent.type(firstNameInput, 'Jayaram');
-    userEvent.type(lastNameInput, 'Nair');
-    userEvent.type(emailInput, 'jsnair46@gmail.com');
-    userEvent.type(messageInput, 'I am a Coder!');
+    //Act - type into the form , filling out all the fields and then submit
+    userEvent.type(firstNameInput, "Jayaram");
+    userEvent.type(lastNameInput, "Nair");
+    userEvent.type(emailInput, "jsnair46@gmail.com");
+    userEvent.type(messageInput, 'I am a coder');
+    userEvent.click(submitButton);
 
+    // Assert - make sure that the new contact we created is displayed on the screen
+    const firstName = screen.findByText(/jayaram/i);
+
+    firstName.then((element) => {
+        expect(element).toBeInTheDocument();
+    })
 })
